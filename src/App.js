e881d4495df69a7aded69da03fe51d9069849024
pscroce <pscroce@gmail.com>
2018-10-14 13:15:51 -0400

    feat: improve card sizing on mobile

--- src/App.js
+++ src/App.js
@@ -28,16 +28,7 @@ class App extends Component {
         </header>
         <body>
           <div className="section">
-            <h1>Peter Croce makes things</h1>
-            <div className="text-section">
-              <p>Hello. I am a curious, friendly, adventurous person who likes to make things.
-                I work as a product manager, currently with the good people at <a
-                  href="https://postlight.com" target="_blank" rel="noopener noreferrer nofollow"
-                  >Postlight</a> in New York City, and formerly with the design-first digital studio, <a
-                  href="https://www.ny-ave.com/" target="_blank" rel="noopener noreferrer nofollow"
-                  >New York Ave</a>.
-               </p>
-            </div>
+            <h1>Peter Croce</h1>
           </div>
           <div className="section">
             <h2 className="no-margin-bottom">Some of those things are listed here</h2>
