eb521630c710679f60e37e780ede7767825abdf1
pscroce <pscroce@gmail.com>
2019-04-23 12:44:46 -0400

    feat: update copy and remove expired .nyc domains from links to Meetup

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
               className="github">GitHub</a> */}
             <a href="https://linkedin.com/in/pscroce" target="_blank" rel="noopener noreferrer"
               className="linkedin">LinkedIn</a>
-            <a href="https://waitbutwhy.nyc" target="_blank" rel="noopener noreferrer"
+            <a href="https://www.meetup.com/Wait-But-Why-NYC" target="_blank" rel="noopener noreferrer"
               className="meetup">Meetup</a>
           </div>
         </header>
@@ -33,19 +33,21 @@ class App extends Component {
 
           <div className="section -paragraph">
             <p>I currently work with the good people at <a href="https://postlight.com" target="_blank" rel="noopener noreferrer">Postlight</a> in New York City.</p>
-            <p>Prior, I worked for <a href="https://www.ny-ave.com/" target="_blank" rel="noopener noreferrer">New York Ave</a> and <a href="https://thedali.org/innovation" target="_blank" rel="noopener noreferrer">The Dali Museum Innovation Labs</a>, slowly work-traveled through Europe and the Caribbean, and served as an AmeriCorps volunteer measuring businesses' environmental impact and offering custom cost-cutting sustainable options.</p>
+            <p>Prior, I worked with <a href="https://www.ny-ave.com/" target="_blank" rel="noopener noreferrer">New York Ave</a> and <a href="https://thedali.org/innovation" target="_blank" rel="noopener noreferrer">The Dali Museum Innovation Labs</a> and served in AmeriCorps measuring businesses' environmental impact and offering custom efficiency recommendations. </p>
+            <p>Before that, I slowly work-traveled and hitchiked through parts of Europe, lived and worked on a sailboat and made <a href="https://www.youtube.com/watch?v=UCwNYGvzR5E&t" target="_blank" rel="noopener noreferrer">a video that excludes all the hard and gross parts</a>, <a href="https://www.youtube.com/watch?v=C-OcMKMY23I" target="_blank" rel="noopener noreferrer">teleported around Scotland and Wales</a>, and got degrees at University of Florida in psychology and criminology.</p>
+            <p>I like people a lot and I like thinking about what sorts of experiences people find useful and fulfilling. Right now, I'm most focused on how technology fits there.</p>
           </div>
 
           <div className="section -paragraph">
             <h2>Where I come from and where I'm going</h2>
-            <p>My product work so far has involved finance, media, and non-profit verticals in which I've worked with teams of incredibly smart and talented people to create two-sided marketplaces, publishing platforms, and even a birdwatching app.</p>
-            <p>By listening to stakeholders, I turn undefined or roughly-defined goals into strategies and tactics that are actionable by design and engineering experts to quickly ship, learn, and iterate on products and their supporting platforms.</p>
+            <p>My product work so far has involved financial, media, and non-profit verticals. I've had the pleasure of working with teams of incredibly talented people to create marketplaces, publishing platforms, and even a birdwatching app.</p>
+            <p>By listening to and working directly with stakeholders, I turn undefined or roughly-defined goals into strategies and tactics to create excellent experiences and data-rich, API-driven applications that always ship.</p>
             <p>I do the work I do because I plan to use business and technology as a force for not just doing well but doing good.</p>
           </div>
           <div className="section -paragraph">
             <h2>How I'm getting there</h2>
             <p>Wherever I go I like to build community around shared interests. In 2016, I co-founded Overlap, a non-profit organization that helps local businesses qualify and apply for B Corporation certification. By involving Stetson University in the effort, we were able to involve passionate students and ensure the continuation of the organization’s mission.</p>
-            <p>In New York, I started <a href="https://waitbutwhy.nyc" target="_blank" rel="noopener noreferrer">Wait But Why NYC</a>, a group that gets together to talk about some of the big, unwieldy topics covered on the blog <a href="https://waitbutwhy.com">Wait But Why</a>. I also participate in StartingBloc, an international community of socially minded entrepreneurs, and Global Shapers, a World Economic Forum association that brings young people together to design and implement community projects around the world.</p>
+            <p>In New York, I started <a href="https://www.meetup.com/Wait-But-Why-NYC" target="_blank" rel="noopener noreferrer">Wait But Why NYC</a>, a group that gets together to talk about some of the big, unwieldy topics covered on the blog <a href="https://waitbutwhy.com">Wait But Why</a>. I also participate in StartingBloc, an international community of socially minded entrepreneurs, and Global Shapers, a World Economic Forum association that brings young people together to design and implement community projects around the world.</p>
           </div>
           <div className="section -paragraph">
             <h2>Say Hello: <a href="mailto:pscroce@gmail.com">pscroce@gmail.com</a></h2>
