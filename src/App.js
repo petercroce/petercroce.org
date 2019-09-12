46c07ff81a02a81953ef5eaddfa9ff56f2cbd93d
pscroce <pscroce@gmail.com>
2019-09-11 21:48:38 -0400

    feat: fix typo and add Reversible and biomimicry Instagram

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
             <h1>Peter Croce</h1>
             <p>I'm a senior product manager currently work with the good people at <a href="https://postlight.com" target="_blank" rel="noopener noreferrer">Postlight</a> in New York City.</p>
             <p>I have also worked as an innovation lab facilitator for <a href="https://thedali.org/innovation" target="_blank" rel="noopener noreferrer">The Dali Museum</a> and served as an AmeriCorps volunteer auditing businesses' environmental impact and offering custom efficiency roadmaps. </p>
-            <p>Before that, I slowly work-traveled and hitchiked through parts of Europe while working freelance as a front-end software engineer, <a href="https://www.youtube.com/watch?v=UCwNYGvzR5E&t" target="_blank" rel="noopener noreferrer">lived and worked on a sailboat</a>, and <a href="https://www.youtube.com/watch?v=C-OcMKMY23I" target="_blank" rel="noopener noreferrer">teleported around Scotland and Wales</a>.</p>
+            <p>Before that, I slowly work-traveled and hitchhiked through parts of Europe while working freelance as a front-end software engineer, <a href="https://www.youtube.com/watch?v=UCwNYGvzR5E&t" target="_blank" rel="noopener noreferrer">lived and worked on a sailboat</a>, and <a href="https://www.youtube.com/watch?v=C-OcMKMY23I" target="_blank" rel="noopener noreferrer">teleported around Scotland and Wales</a>.</p>
             <p>I am drawn to business and technology because I believe the two together are the biggest levers to effect social good.</p>
           </div>
 
@@ -51,13 +51,19 @@ class App extends Component {
             <h2>Side projects & community</h2>
             <ul>
               <li>
+                Making tools for reversing climate change with Reversible
+              </li>
+              <li>
                 Hosting meetups for NYC-based product managers at <a href="https://postlight.com/events" target="_blank" rel="noopener noreferrer">Postlight</a>
               </li>
               <li>
                 Hosting meetups for readers of <a href="https://waitbutwhy.com" target="_blank" rel="noopener noreferrer">Wait But Why</a> at <a href="https://www.meetup.com/Wait-But-Why-NYC" target="_blank" rel="noopener noreferrer">Wait But Why NYC</a>
               </li>
               <li>
-                Co-founded Overlap, a non-profit organization that helps local businesses qualify and apply for B Corporation certification
+                Shared an example of biology inspring innovation every week for two years as @biomimicry on Instagram
+              </li>
+              <li>
+                Co-founded Overlap, a non-profit organization that helps Central Florida businesses qualify and apply for B Corporation certification
               </li>
               <li>
                 <a href="https://startingbloc.org/" target="_blank" rel="noopener noreferrer">StartingBloc</a>
@@ -90,7 +96,7 @@ class App extends Component {
           Peter lives in Brooklyn with his partner, <a
             href="https://erinpeace.com"
             target="_blank"
-            rel="noopener noreferrer nofollow"
+            rel="noopener noreferrer"
               >Erin</a>.
         </p>
       </footer>
