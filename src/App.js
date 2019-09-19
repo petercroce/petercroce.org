a19b0276005c3100a585abb3a38738d87650dab4
pscroce <pscroce@gmail.com>
2019-09-18 21:22:24 -0400

    feat: change welcome section to remove Dali museum and add reversing climate change

--- src/App.js
+++ src/App.js
@@ -17,9 +17,9 @@ class App extends Component {
             <img src={me} alt="Headshot of Peter Croce" id="headshot" />
             <h1>Peter Croce</h1>
             <p>I'm a senior product manager currently work with the good people at <a href="https://postlight.com" target="_blank" rel="noopener noreferrer">Postlight</a> in New York City.</p>
-            <p>I have also worked as an innovation lab facilitator for <a href="https://thedali.org/innovation" target="_blank" rel="noopener noreferrer">The Dali Museum</a> and served as an AmeriCorps volunteer auditing businesses' environmental impact and offering custom efficiency roadmaps. </p>
-            <p>Before that, I slowly work-traveled and hitchhiked through parts of Europe while working freelance as a front-end software engineer, <a href="https://www.youtube.com/watch?v=UCwNYGvzR5E&t" target="_blank" rel="noopener noreferrer">lived and worked on a sailboat</a>, and <a href="https://www.youtube.com/watch?v=C-OcMKMY23I" target="_blank" rel="noopener noreferrer">teleported around Scotland and Wales</a>.</p>
-            <p>I am drawn to business and technology because I believe the two together are the biggest levers to effect social good.</p>
+            <p>Before working in technology, I served as an AmeriCorps volunteer auditing businesses' environmental impact and offering custom efficiency improvement roadmaps.</p>
+            <p>Before that, I slowly work-traveled and hitchhiked through parts of Europe while working freelance as a software engineer, <a href="https://www.youtube.com/watch?v=UCwNYGvzR5E&t" target="_blank" rel="noopener noreferrer">lived and worked on a sailboat</a>, and <a href="https://www.youtube.com/watch?v=C-OcMKMY23I" target="_blank" rel="noopener noreferrer">teleported around Scotland and Wales</a>.</p>
+            <p>I am drawn to business and technology because I believe they are the biggest levers to effect social good, starting with reversing climate change.</p>
           </div>
 
         </div>
